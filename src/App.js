fd1b93a0d72052e2413961ff5fbe40bfba2d4640
Chrisdocs <chrisa.wolfe@gmail.com>
2021-12-10 15:25:13 -0600

    header skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import WorldMap from './pages/WorldMap/Worldmap';
+import Header from './components/Sections/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <WorldMap></WorldMap>
+      <section>
+        <Header></Header>
+      </section>
+      <section>
+        <WorldMap></WorldMap>
+      </section>
     </div>
   );
 }
